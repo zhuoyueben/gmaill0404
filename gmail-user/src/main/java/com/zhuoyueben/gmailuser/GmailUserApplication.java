e330be1dc6ac82aa84743cfeb12d96c9bf0909bd
zhouyueben <2487186110@qq.com>
2020-04-04 11:58:32 +0800

    commit demo

---
+++ gmail-user/src/main/java/com/zhuoyueben/gmailuser/GmailUserApplication.java
@@ -0,0 +1,13 @@
+package com.zhuoyueben.gmailuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmailUserApplication.class, args);
+    }
+
+}
